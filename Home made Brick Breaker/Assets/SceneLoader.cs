ea7f8555d0b5a7148f92f9f2491c2ee8853a474b
ThatGuy7051 <lukasz.fomin@outlook.com>
2019-10-08 13:27:14 +0100

    First Attempt

---
+++ Home made Brick Breaker/Assets/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScenePosition()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(currentSceneIndex);
+    }
+
+     public void LoadStartScene()
+     {
+         SceneManager.LoadScene(0);
+     }
+
+     public void LoadSettingsScene()
+     {
+         SceneManager.LoadScene(1);
+     }
+     public void LoadPauseScene()
+     {
+         SceneManager.LoadScene(2);
+     }
+     public void LoadMultiplayerScene()
+     {
+         SceneManager.LoadScene(3);
+     }
+     public void LoadLeaderboardScene()
+     {
+         SceneManager.LoadScene(4);
+     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
